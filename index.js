fde6bed7db570682b7db6cca9b579b23301cc1db
conormgomes <gomesconor@gmail.com>
2024-10-10 18:15:29 -0700

    Testin change to response json and added k8s updating to workflow (hopefully)

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.get('/', async (request, response) => {
 
     response.json({ message: "My name is Conor", 
                     timestamp: Date.now(),
-                    k8s: "updated"});
+                    k8s: "updated",
+                    workflow: "working"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
