63537343fc6e3e3ddab104b784dd38685bd8e60f
conormgomes <gomesconor@gmail.com>
2024-10-18 16:56:29 -0700

    Changed logic of k8s deployment to run even if docker job was skipped

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const app = express();
 app.get('/', async (request, response) => {
 
     response.json({ message: "My name is Conor", 
-                    timestamp: Date.now(),
-                    test: "VAR"});
+                    timestamp: Date.now()});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
