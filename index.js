dc2bb2e0cdd92ab5237ba63b1e0c1cf371d4836a
conormgomes <gomesconor@gmail.com>
2024-10-18 16:29:10 -0700

    Testing with change to index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/', async (request, response) => {
 
     response.json({ message: "My name is Conor", 
-                    timestamp: Date.now()});
+                    timestamp: Date.now(),
+                    test: "Var"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
