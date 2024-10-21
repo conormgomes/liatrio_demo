88562855d070024d0b77969136b1ca63f4690281
conormgomes <gomesconor@gmail.com>
2024-10-21 14:48:44 -0700

    Live demo push

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const app = express();
 app.get('/', async (request, response) => {
 
     response.json({ message: "My name is Conor", 
-                    timestamp: Date.now()});
+                    timestamp: Date.now(),
+                liatrio: "hi"});
 });
 
 app.listen(80, () => console.log('App available on http://localhost:80'));
